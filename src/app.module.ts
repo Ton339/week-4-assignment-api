6d26462d6873b18f21066056fa459d79e5237a2a
Ton339 <sithachad.krachaisri@gmail.com>
2026-05-11 11:06:41 +0700

    feat: implement user module with CRUD endpoints and TypeORM entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskModule } from './task/task.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { TaskModule } from './task/task.module';
       synchronize: true,
     }),
     TaskModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,50 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  role: string;
+
+  @Column()
+  status: string;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  birthdate: Date;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  occupation: string;
+
+  @CreateDateColumn()
+  created_at: Date; // Creation date
+
+  @UpdateDateColumn()
+  updated_at: Date; // Last updated date
+
+  @DeleteDateColumn()
+  deleted_at: Date; // Deletion date
+}
